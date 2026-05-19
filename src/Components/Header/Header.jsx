d0fb5205a21924ae5bf9d0118590dd8f343cab7f
HaykTAA <hayktadevosyan335@gmail.com>
2026-05-19 14:20:11 +0400

    profile,add Topic modals

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,6 +4,8 @@ import {useNavigate} from "react-router-dom";
 import {FIRST_PAGE, HOME_PAGE} from "../../pages/routes.jsx";
 import { CgProfile } from "react-icons/cg";
 import i18next from "i18next";
+import {useState} from "react";
+import Modal from "../ui/Modal.jsx";
 
 
 const Header = () => {
@@ -15,6 +17,8 @@ const Header = () => {
     const currentUser = JSON.parse(localStorage.getItem("currentUser"))
     const users = JSON.parse(localStorage.getItem("users")) || []
     const navigate = useNavigate()
+    const [visible, setVisible] = useState(false);
+    const [open, setOpen] = useState(false);
 
     const limit = () => {
         navigate(currentUser ? HOME_PAGE : FIRST_PAGE);
@@ -22,12 +26,14 @@ const Header = () => {
     const handleLogOut = () => {
         localStorage.removeItem('currentUser')
         navigate(FIRST_PAGE)
+        setOpen(false);
     }
     const handleDelete = () => {
         const filteredUsers = users.filter(e => e.id !== currentUser.id)
         localStorage.setItem("users", JSON.stringify(filteredUsers))
         localStorage.removeItem('currentUser')
         navigate(FIRST_PAGE)
+        setOpen(false);
     }
     return (
         <div className="h-15 w-full flex justify-between items-center gap-6 px-78">
@@ -51,7 +57,7 @@ const Header = () => {
                 {
                     currentUser && (
                         <div className="flex gap-5">
-                            <div className="flex gap-2 items-center">
+                            <div className="flex gap-2 items-center" onClick={() => setOpen(true) }>
                                 <CgProfile className="size-6" />
                                 <div className=" text-xl capitalize">
                                     {
@@ -59,44 +65,60 @@ const Header = () => {
                                     }
                                 </div>
                             </div>
-                            <div className="flex gap-3">
-                                <Button
-                                    onClick={handleLogOut}
-                                    className={"font-bold"}
-                                >
-                                    Logout
-                                </Button>
 
-                                <Button
-                                    onClick={handleDelete}
-                                    className={"font-bold"}
-                                >
-                                    Delete
-                                </Button>
-                            </div>
                         </div>
                     )
                 }
+                {open && currentUser && (
+                    <Modal
+                        setVisible={setOpen}
+                    >
+                        <div className="flex gap-3">
+                            <Button
+                                onClick={handleLogOut}
+                                className={"font-bold"}
+                            >
+                                Logout
+                            </Button>
 
+                            <Button
+                                onClick={handleDelete}
+                                className={"font-bold"}
+                            >
+                                Delete
+                            </Button>
+                        </div>
+                    </Modal>)
+                }
             </div>
-            {currentUser ? <Button
+            {currentUser ?
+                <Button
                 className={"bg-black  text-white rounded-lg  px-5 py-4 text-l font-bold"}
                 children={`+ Add Topic`}
-            /> : languages.map(el => {
-                return (
-                    <div
-                        className=' border rounded-3xl cursor-pointer'
-                        key={el.id}
-                        onClick={() => i18next.changeLanguage(el.language)}
-                    >
-                        <img
-                            className ="h-10 w-10 rounded-full"
-                            src={el.img}
-                            alt={el.language}
-                        />
-                    </div>
-                )
-            })}
+                onClick={() => setVisible(true)}
+                />
+                :
+                languages.map(el => {
+                    return (
+                        <div
+                            className=' border rounded-3xl cursor-pointer'
+                            key={el.id}
+                            onClick={() => i18next.changeLanguage(el.language)}
+                        >
+                            <img
+                                className ="h-10 w-10 rounded-full"
+                                src={el.img}
+                                alt={el.language}
+                            />
+                        </div>
+                    )
+                })
+            }
+            {visible && (
+                <Modal setVisible={setVisible}>
+
+                </Modal>
+            )}
         </div>
     );
 };
--- src/Components/ui/Modal.jsx
+++ src/Components/ui/Modal.jsx
@@ -1,10 +1,11 @@
-const Modal = ({ setVisible, children }) => {
+const Modal = ({ setVisible, children ,style}) => {
     return (
         <div
             className="fixed inset-0 bg-black/70 flex justify-center items-center"
-            onClick={() => setVisible()}
+            onClick={() => setVisible(false)}
         >
             <div
+                style={{}}
                 className="w-[1000px] min-h-[400px] max-h-[600px] overflow-auto flex flex-col gap-3 bg-white rounded-md p-5"
                 onClick={(e) => e.stopPropagation()}
             >
